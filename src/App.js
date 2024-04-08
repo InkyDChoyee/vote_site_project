a6cd54a5f22f037e8da6ffddde2db631c2159bf1
InkyDChoyee <choyee0421@naver.com>
2024-04-08 13:51:29 +0900

    Feat: create vote

--- src/App.js
+++ src/App.js
@@ -1,64 +1,12 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
+import CreateVote from "./components/CreateVote";
+import VoteList from "./components/VoteList";
 
 function App() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [votes, setVotes] = useState([]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.post("http://localhost:5000/vote", { title, content });
-      alert("투표가 저장되었습니다.");
-      fetchVotes(); // 투표가 저장된 후 투표 목록 다시 불러오기
-    } catch (error) {
-      console.error("투표 저장 실패:", error);
-    }
-  };
-
-  async function fetchVotes() {
-    try {
-      const response = await axios.get("http://localhost:5000/votes");
-      setVotes(response.data);
-    } catch (error) {
-      console.error("투표 목록 가져오기 실패:", error);
-    }
-  }
-
-  useEffect(() => {
-    fetchVotes(); // 초기 렌더링 시 투표 목록 불러오기
-  }, []);
-
   return (
     <div>
-      <h1>투표 만들기</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          제목:
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          내용:
-          <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type="submit">저장</button>
-      </form>
-      <h2>투표 목록</h2>
-      <ul>
-        {votes.map((vote) => (
-          <li key={vote._id}>{vote.title}</li>
-        ))}
-      </ul>
+      <CreateVote />
+      <VoteList />
     </div>
   );
 }
