77378163ad00a91f065bcb368b2e5acde5fa7977
InkyDChoyee <choyee0421@naver.com>
2024-04-09 12:49:24 +0900

    Feat: vote option input

--- src/components/CreateVote.js
+++ src/components/CreateVote.js
@@ -3,16 +3,28 @@ import axios from "axios";
 
 function CreateVote() {
   const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState([]);
+  const [newOption, setNewOption] = useState("");
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); // 이벤트의 기본 동작을 중지하는 함수 = 이후에 원하는 동작 추가 가능
     try {
       await axios.post("http://localhost:5000/vote", { title, content });
       alert("투표가 저장되었습니다.");
     } catch (error) {
       console.error("투표 저장 실패:", error);
     }
+  };
+
+  const handleOptionChange = (index, value) => {
+    const updatedOptions = [...content];
+    updatedOptions[index] = value;
+    setContent(updatedOptions);
+  };
+
+  const handleAddOption = () => {
+    setContent([...content, newOption]);
+    setNewOption("");
   };
 
   return (
@@ -29,11 +41,26 @@ function CreateVote() {
         </label>
         <br />
         <label>
-          내용:
-          <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          투표 항목:
+          {content.map((option, index) => (
+            <div key={index}>
+              <input
+                type="text"
+                value={option}
+                onChange={(e) => handleOptionChange(index, e.target.value)}
+              />
+            </div>
+          ))}
+          <div>
+            <input
+              type="text"
+              value={newOption}
+              onChange={(e) => setNewOption(e.target.value)}
+            />
+            <button type="button" onClick={handleAddOption}>
+              +
+            </button>
+          </div>
         </label>
         <br />
         <button type="submit">저장</button>
