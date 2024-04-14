9e2d95ce977faa8646a7f287081ab7bf52d52fbd
InkyDChoyee <choyee0421@naver.com>
2024-04-14 13:59:50 +0900

    Style: vote list css

--- src/components/VoteList.js
+++ src/components/VoteList.js
@@ -1,19 +1,23 @@
 // VoteList.js
 import React from "react";
 import axios from "axios";
+import "./VoteList.css";
 
 function VoteList({ fetchVotes, votes, onSelectVote }) {
   return (
     <div>
-      <h2>투표 목록</h2>
-      <ul>
-        {votes.map((vote) => (
-          <li key={vote._id}>
-            {/* 투표 항목 클릭 시 onSelectVote 호출 */}
-            <p onClick={() => onSelectVote(vote._id)}>{vote.title}</p>
-          </li>
-        ))}
-      </ul>
+      <h1>투표 목록</h1>
+      <div className="list">
+        <ul>
+          {votes.map((vote) => (
+            <li key={vote._id}>
+              <button onClick={() => onSelectVote(vote._id)}>
+                {vote.title}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
