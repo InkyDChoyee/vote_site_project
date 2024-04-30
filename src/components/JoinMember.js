9ec3bffb661c4d68371c1769e6a6b9907f6387f7
InkyDChoyee <choyee0421@naver.com>
2024-04-30 17:34:52 +0900

    Feat: password confirm logic

--- src/components/JoinMember.js
+++ src/components/JoinMember.js
@@ -4,9 +4,16 @@ import axios from 'axios';
 const JoinMember = () => {
   const [userID, setUserID] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (password !== passwordConfirm) {
+      alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
+      return;
+    }
+
     try {
       await axios.post('http://localhost:5000/join', {
         userID,
@@ -28,7 +35,9 @@ const JoinMember = () => {
             type="text"
             value={userID}
             onChange={(e) => setUserID(e.target.value)}
-            placeholder="아이디"></input>
+            placeholder="아이디"
+            required
+          />
         </div>
         <div className="password_box">
           <input
@@ -36,11 +45,17 @@ const JoinMember = () => {
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="비밀번호"></input>
+            placeholder="비밀번호"
+            required
+          />
           <input
             className="password_confirm"
             type="password"
-            placeholder="비밀번호 확인"></input>
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+            placeholder="비밀번호 확인"
+            required
+          />
         </div>
         <button className="save_btn" type="submit">
           가입
